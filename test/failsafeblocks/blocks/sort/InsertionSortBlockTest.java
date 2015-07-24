891e5f2fcdc36d9c836e5319b6db721b0ef3b428
Iván Pondal <ivan.pondal@gmail.com>
2015-07-24 18:15:59 -0300

    Add insertion sort block tests

--- test/failsafeblocks/blocks/sort/InsertionSortBlockTest.java
+++ test/failsafeblocks/blocks/sort/InsertionSortBlockTest.java
@@ -8,41 +8,40 @@ import java.util.List;
 import org.junit.Before;
 import org.junit.Test;
 
-public class MergeSortBlockTest {
+public class InsertionSortBlockTest {
 
-	private MergeSortBlock<Integer> mergeSortBlock;
+	private InsertionSortBlock<Integer> insertionSortBlock;
 
 	@Before
 	public void setUp(){
-		mergeSortBlock = new MergeSortBlock<Integer>();
+		insertionSortBlock = new InsertionSortBlock<Integer>();
 	}
 
 	@Test
 	public void testOrderedList() {
 		List<Integer> input = Arrays.asList(1, 2, 3, 4);
 
-		assertEquals("[1, 2, 3, 4]", mergeSortBlock.process(input).toString());
+		assertEquals("[1, 2, 3, 4]", insertionSortBlock.process(input).toString());
 	}
 
 	@Test
 	public void testUnorderedList(){
 		List<Integer> input = Arrays.asList(4, 2, 1, 3);
 
-		assertEquals("[1, 2, 3, 4]", mergeSortBlock.process(input).toString());
+		assertEquals("[1, 2, 3, 4]", insertionSortBlock.process(input).toString());
 	}
 
 	@Test
 	public void testUnevenSizeOrderedList(){
 		List<Integer> input = Arrays.asList(1, 2, 3);
 
-		assertEquals("[1, 2, 3]", mergeSortBlock.process(input).toString());
+		assertEquals("[1, 2, 3]", insertionSortBlock.process(input).toString());
 	}
 
 	@Test
 	public void testUnvenSizeUnorderedList(){
 		List<Integer> input = Arrays.asList(2, 3, 1);
 
-		assertEquals("[1, 2, 3]", mergeSortBlock.process(input).toString());
+		assertEquals("[1, 2, 3]", insertionSortBlock.process(input).toString());
 	}
-
 }
